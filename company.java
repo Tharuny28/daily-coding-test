5930eb5813dd756e134b834f1d85f98fbc587f92
TharunY <tharun28y@gmail.com>
2025-03-11 14:40:32 +0530

    Create company.java

---
+++ company.java
@@ -0,0 +1,23 @@
+public class Company {
+		static String CompanyName;
+		String employeename;
+		
+		
+		public void Company(String ComName,String employee){
+			this.CompanyName = ComName;
+			this.employeename = employee;
+			
+		}
+		public void display() {
+			System.out.println(CompanyName);
+			System.out.println(employeename);
+		}
+		
+		public static void main(String[] args) {
+			Company c = new Company();
+			c.Company("Farman pvt Ltd", "Farman");
+			c.display();
+			
+		}
+
+}
